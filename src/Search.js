d3c22b61ac5096815b71ae597c4c80d6d609c9b9
cerlans <kgomez222@hotmail.com>
2021-02-12 12:01:51 -0500

    1 file modified

--- src/Search.js
+++ src/Search.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import React, { useState,useEffect } from 'react';
+import React, {useState} from 'react';
+import "./style.css";
 
-const Search = function Search(){
-   const [searchString,setString] = useState(' ');
+
+export default function App() {
+  const [searchString,setString] = useState(' ');
   const [data,setData] = useState([])
-  const [deep,setDeep] = useState([])
   const [query,setQuery] = useState('');
- useEffect (() => {
-fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchString}&maxResults=20&key=AIzaSyCFiBdff1JxkTe4F_0auryiuqiYMIJd48g`)
+
+function getData(){
+  setQuery(searchString)
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchString}&maxResults=30&key=AIzaSyCFiBdff1JxkTe4F_0auryiuqiYMIJd48g`)
   .then(function(response){
     return response.json()
-  })
-  .then(function(data){
-   let test = data.items.map(function(value){
-     return value.volumeInfo
-    })
-  return test
-  }).then(function(data){
-   let componentData = data.map(function(value){
-     return value.title;
-   })
-   return componentData
   }).then(function(data){
     console.log(data);
+    setData(data.items)
   })
- },[query]);
-function test(){
-  console.log(data)
-}
+ }
+ // i have to create a component that will map the above values out, it will be  a sort off 'card' that will dispaly the basic book information and the book thumbnail
+ // how do i handle pagination and load more results  in? if you search up  a book you get 20 results back, but not 1k, which is the total amount of results returned
+ // i can create a helper function that will break down the author array, the authors property is within an array due to the fact that there can be multiple authors in a book, i need to find out  a way to map out the authors, destructuring? basic mapping like im already doing above?
   return (
     <>
     <div className='heading'>
@@ -35,12 +27,33 @@ function test(){
       You dont need to know the name of the book, if you know the name of the author or publisher, just type, search and find </h2>
     </div>
     <div className='search'>
-      <input type='text' placeHolder='Type Auther,BookName,ISBN...' onChange = {function(event){
+      <input type='text' placeholder='Type Auther,BookName,ISBN...' onChange = {function(event){
         setString(event.target.value); }}/>
-      <button onClick={()=> setQuery(searchString)}>Search</button>
+     <button onClick={getData}>Button</button>
+      </div>
+      <div>
+      {data.map(function(value,index){
+        return (
+          <>
+              <div className='bookCard' key={index}>
+                    <div className='thumbnail'>
+                    <img src={console.log(value.volumeInfo.imageLinks.indexOf)} />
+                    </div>
+                    <div className='content'>
+                      <h2>{value.volumeInfo.title}</h2>
+                      <p className='subTitle'> {value.volumeInfo.subtitle}</p>
+                      <p>{`Authors: ${value.volumeInfo.authors ? value.volumeInfo.authors:'No author found'}`}</p>
+                      <p>{`Publisher: ${value.volumeInfo.publisher ? value.volumeInfo.publisher:'No publisher found'}`}</p>
+                      <p>{`Published Date: ${value.volumeInfo.publishedDate ? value.volumeInfo.publishedDate:'No date found'}`}</p>
+
+                    </div>
+              </div>
+         
+          </>
+        )
+      })}
       </div>
     </>
   )
+
 }
-console.log('tit')
-export default Search;
